2c93153ba0b353469977a880fe77eb2fd9835c41
DevJonathanSantos <jojortd0@gmail.com>
2024-07-13 19:33:29 -0300

    add poc

---
+++ src/app/api/user/[id]/route.ts
@@ -0,0 +1,50 @@
+import { NextResponse, NextRequest } from 'next/server'
+
+const users = [
+    {
+        id: 1,
+        name: 'Jonathan',
+        email: 'jonathan@example.com',
+        age: 28,
+        city: 'New York'
+    },
+    {
+        id: 2,
+        name: 'Serena',
+        email: 'serena@example.com',
+        age: 32,
+        city: 'Los Angeles'
+    },
+    {
+        id: 3,
+        name: 'Miriane',
+        email: 'miriane@example.com',
+        age: 25,
+        city: 'Chicago'
+    },
+    {
+        id: 4,
+        name: 'Carlos',
+        email: 'carlos@example.com',
+        age: 30,
+        city: 'Miami'
+    },
+    {
+        id: 5,
+        name: 'Alicia',
+        email: 'alicia@example.com',
+        age: 27,
+        city: 'San Francisco'
+    },
+    { id: 6, name: 'David', email: 'david@example.com', age: 35, city: 'Boston' },
+    { id: 7, name: 'Emma', email: 'emma@example.com', age: 22, city: 'Seattle' }
+]
+interface Params {
+    id: number
+}
+
+export async function GET(request: NextRequest, { params }: { params: Params }) {
+    const user = users.find((x) => x.id == params.id)
+
+    return NextResponse.json(user)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,103 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
+import Image from 'next/image'
+import styles from './page.module.css'
+import { useEffect, useState } from 'react'
+
+interface User {
+    id: number
+    name: string
+    email: string
+    age: number
+    city: string
+}
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const [users, setUsers] = useState<User[]>([])
+    const [user, setUser] = useState<User | null>(null)
+    const [loading, setLoading] = useState(true)
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    async function fetchUsers() {
+        try {
+            const response = await fetch('/api/user')
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status}`)
+            }
+            const data = await response.json()
+            setUsers(data.users)
+        } catch (err) {
+        } finally {
+            setLoading(false)
+        }
+    }
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+    async function getById(id: number) {
+        try {
+            const res = await fetch(`/api/user/${id}`)
+            if (!res.ok) {
+                throw new Error(`Error: ${res.status}`)
+            }
+            const user: User = await res.json()
+            setUser(user)
+        } catch (err) {
+        } finally {
+            setLoading(false)
+        }
+    }
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+    useEffect(() => {
+        fetchUsers()
+    }, [])
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+    if (loading) {
+        return <div>Loading...</div>
+    }
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    return (
+        <main className={styles.main}>
+            <div className={styles.description}>
+                <ul>
+                    {users.map((user) => (
+                        <li key={user.id} style={{ cursor: 'pointer', margin: '5px' }} onClick={() => getById(user.id)}>
+                            <p>
+                                User:&nbsp;
+                                <code className={styles.code}>{user.name}</code>
+                            </p>
+                        </li>
+                    ))}
+                </ul>
+                <p>
+                    <code className={styles.code}>
+                        {' '}
+                        {user && (
+                            <>
+                                {user.name} - {user.email} - {user.age} - {user.city}
+                            </>
+                        )}
+                    </code>
+                </p>
+                <div>
+                    <a
+                        href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        By{' '}
+                        <Image
+                            src="/vercel.svg"
+                            alt="Vercel Logo"
+                            className={styles.vercelLogo}
+                            width={100}
+                            height={24}
+                            priority
+                        />
+                    </a>
+                </div>
+            </div>
+
+            <div className={styles.center}>
+                <Image className={styles.logo} src="/next.svg" alt="Next.js Logo" width={180} height={37} priority />
+            </div>
+        </main>
+    )
 }
